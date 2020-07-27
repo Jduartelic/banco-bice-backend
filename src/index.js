8ff208bdfd08497fbdd6e50ad8948b6891082af4
jduarte <jduartelic@gmail.com>
2020-07-26 20:53:32 -0400

    feat(jd): add handler api

--- src/index.js
+++ src/index.js
@@ -1,20 +1,15 @@
+require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
-const fetch = require("node-fetch");
-const config = require("./config/");
-require("dotenv").config();
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+const InfoDetailIndexMarketplace = require("./api/InfoDetailIndexMarketplace/api");
 
-console.log(process.env.PORT);
+app.listen(process.env.PORT, () => {});
 
-app.listen(process.env.PORT, () => console.log("listening on port 3030!"));
+app.get("/last", InfoDetailIndexMarketplace.getDetailsInfoLast);
 
-app.get("/", async (req, res) => {
-  const response = await fetch(config.API_URL + "/last");
-  const json = await response.json();
-  res.send(json);
-});
+app.post("/info", InfoDetailIndexMarketplace.getDetailsInfoValues);
